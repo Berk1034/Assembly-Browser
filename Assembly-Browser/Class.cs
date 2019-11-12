aac600c6ce64bcec0c3343362d087f3acb7fbc31
Constantin Hitov <kostya.khitov@mail.ru>
2019-11-12 14:30:27 +0300

    Implement Method-browsering

--- Assembly-Browser/Class.cs
+++ Assembly-Browser/Class.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +11,25 @@ namespace Assembly_Browser
     public class Class
     {
         public string Name { get; set; }
+        public ObservableCollection<Method> Methods { get; set; }
 
         public Class(Type type)
         {
             Name = type.Name;
+            Methods = new ObservableCollection<Method>();
+            GetMethods(type);
+        }
+
+        public void GetMethods(Type type)
+        {
+            MethodInfo[] methods = type.GetMethods();
+            ObservableCollection<Method> tmp = new ObservableCollection<Method>();
+            foreach (MethodInfo methodInfo in methods)
+            {
+                Method method = new Method(methodInfo);
+                tmp.Add(method);
+            }
+            Methods = tmp;
         }
     }
 }
---
+++ Assembly-Browser/Method.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly_Browser
+{
+    public class Method
+    {
+        public string Signature { get; set; }
+
+        public Method(MethodInfo methodinfo)
+        {
+            String[] param = methodinfo.GetParameters().Select(p => String.Format("{0} {1}", p.ParameterType.Name, p.Name)).ToArray();
+            Signature = String.Format("{0} {1} ({2})", methodinfo.ReturnType.Name, methodinfo.Name, String.Join(",", param));
+        }
+    }
+}
