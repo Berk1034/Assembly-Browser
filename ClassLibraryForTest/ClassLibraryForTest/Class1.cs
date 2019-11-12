90b304a756dfbaffe70df7f11785103ef736cef8
Constantin Hitov <kostya.khitov@mail.ru>
2019-11-12 23:17:12 +0300

    Add ClassLibraryForTest as another dll for AssemblyBrowser

---
+++ ClassLibraryForTest/ClassLibraryForTest/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryForTest
+{
+    public class Class1
+    {
+        public int a;
+        public bool b { get; set; }
+        public void Create(int a)
+        {
+            this.a = a;
+            b = true;
+        }
+        public Class1(int a, bool b)
+        {
+
+        }
+        internal string c = "Lol";
+    }
+}
