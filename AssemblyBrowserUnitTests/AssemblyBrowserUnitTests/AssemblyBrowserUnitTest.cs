d50b627ae54380804ffc063ed3c93bf898675f45
Constantin Hitov <kostya.khitov@mail.ru>
2019-11-12 23:15:15 +0300

    Add AssemblyBrowserUnitTests with AssemblyBrowserLibrary.dll simple tests

---
+++ AssemblyBrowserUnitTests/AssemblyBrowserUnitTests/AssemblyBrowserUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssemblyBrowserLibrary;
+
+namespace AssemblyBrowserUnitTests
+{
+    [TestClass]
+    public class AssemblyBrowserUnitTest
+    {
+        private AssemblyBrowserModel assemblyBrowser;
+        [TestInitialize]
+        public void Initialize()
+        {
+            assemblyBrowser = new AssemblyBrowserModel(AppDomain.CurrentDomain.BaseDirectory + "\\AssemblyBrowserLibrary.dll");
+        }
+
+        [TestMethod]
+        public void DLLLoad_ShouldLoadDLL()
+        {
+            Assert.IsNotNull(assemblyBrowser);
+        }
+
+        [TestMethod]
+        public void NamespaceParse_ShouldReturnOneNamespace()
+        {
+            Assert.AreEqual(1, assemblyBrowser.Namespaces.Count);
+        }
+
+        [TestMethod]
+        public void ClassParse_ShouldReturnAssemblyBrowserModelClass()
+        {
+            Assert.AreEqual("public class AssemblyBrowserModel", assemblyBrowser.Namespaces[0].Classes[0].Name);
+        }
+
+        [TestMethod]
+        public void ClassParse_ShouldReturnOneConstructor()
+        {
+            Assert.AreEqual(1, assemblyBrowser.Namespaces[0].Classes[0].Constructors.Count);
+        }
+
+        [TestMethod]
+        public void ClassParse_ShouldReturnThreeFields()
+        {
+            Assert.AreEqual(3, assemblyBrowser.Namespaces[0].Classes[0].Fields.Count);
+        }
+
+        [TestMethod]
+        public void ClassParse_ShouldReturnThreeMethods()
+        {
+            Assert.AreEqual(3, assemblyBrowser.Namespaces[0].Classes[0].Methods.Count);
+        }
+    }
+}
---
+++ AssemblyBrowserUnitTests/AssemblyBrowserUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssemblyBrowserUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyBrowserUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29f5471f-af9e-489c-a8b1-b618d531c190")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
